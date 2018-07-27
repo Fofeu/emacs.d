2416df40f9c4598ed4f07a3f0cd7764f14cdeaf8
Frédéric Fort <frederic.fort3@univ-tlse3.fr>
2018-07-27 10:10:34 +0200

    remove useless print

--- init.el
+++ init.el
@@ -13,8 +13,6 @@
 
 ;; Load Ocaml libs, if available
 (setq opam-cmd-share (shell-command-to-string "opam config var share 2> /dev/null"))
-(message "a: %s" opam-cmd-share)
-
 (unless (string= "" opam-cmd-share)
   (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
   (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
