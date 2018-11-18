ab8951d61a1939b3e3cf0fdf21241104a90bf200
Frédéric Fort <frederic.fort3@univ-tlse3.fr>
2018-11-18 18:41:30 +0100

    added go-mode package

--- init.el
+++ init.el
@@ -125,7 +125,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-irony yaml-mode smart-tab irony lua-mode browse-kill-ring))))
+    (go-mode flycheck-irony yaml-mode smart-tab irony lua-mode browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
